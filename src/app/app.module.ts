5d4798be802c4d4c6027388c113f506658e63bec
deusmar <!energy89>
2019-05-16 17:40:46 -0300

    49. Adding a Spinner to the Training Screen

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -16,6 +16,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HeaderComponent } from './navigation/header/header.component';
 import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { CurrentTrainingComponent } from './training/current-training/current-training.component';
 
 
 @NgModule({
@@ -28,7 +29,8 @@ import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.com
     PastTrainingComponent,
     WelcomeComponent,
     HeaderComponent,
-    SidenavListComponent
+    SidenavListComponent,
+    CurrentTrainingComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/training/current-training/current-training.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-current-training',
+  templateUrl: './current-training.component.html',
+  styleUrls: ['./current-training.component.css']
+})
+export class CurrentTrainingComponent implements OnInit {
+  progress = 0;
+  constructor() { }
+
+  ngOnInit() {
+    setInterval(() => {
+      this.progress += 1;
+    }, 100)
+  }
+
+}
